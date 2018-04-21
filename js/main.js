96735bf723b5ffc572da662906648709b9a8a92a
BataLaSalade <dsfmarina@gmail.com>
2018-04-22 00:28:23 +0200

    validation for contact form

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
     console.log("bonjour")
     
-    $("#formValidation").submit( function(event) {
+    $("#bookingValidation").submit( function(event) {
+        if (this.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        $(this).addClass('was-validated');
+    });
+
+    $("#contactValidation").submit( function(event) {
         if (this.checkValidity() === false) {
             event.preventDefault();
             event.stopPropagation();
@@ -12,3 +20,4 @@ $(document).ready(function() {
 // Voir avec Marc, j'ai voulu changer le nom de l'ID dans le html
 // j'ai mis le même dans le .js mais le script ne fonctionne plus
 // j'ai remis l'ID précédent et ça ne fonctionne plus non plus...
+
