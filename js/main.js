744eca4f5ea0ff8697f5294e52fac20dbab8bdff
BataLaSalade <dsfmarina@gmail.com>
2018-04-24 14:18:05 +0200

    add main.js script on contact.html and films.html / add scroll to top button in all pages

--- js/main.js
+++ js/main.js
@@ -25,3 +25,20 @@ $(document).ready(function() {
 // j'ai mis le même dans le .js mais le script ne fonctionne plus
 // j'ai remis l'ID précédent et ça ne fonctionne plus non plus...
 
+// scroll to top button
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
